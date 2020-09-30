75d5139ee887cd50caf1897756f6c077349403f8
Misir Jafarov <misir.ceferov@gmail.com>
2020-10-01 02:39:21 +0400

    Remove default Calculator

--- lib/network_logger.dart
+++ lib/network_logger.dart
@@ -5,9 +5,3 @@ export 'src/ui.dart'
     show NetworkLoggerScreen, NetworkLoggerButton, NetworkLoggerOverlay;
 
 export 'src/impl/dio.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
